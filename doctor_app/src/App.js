c4cece4666d599f8ddc83383c783097060e595e5
kowsickram <kowsickram05122001@gmail.com>
2023-07-30 20:48:56 +0530

    Updated Code

--- doctor_app/src/App.js
+++ doctor_app/src/App.js
@@ -1,65 +1,76 @@
 
 import './App.css';
 import "./styles.css"
-import React, { useState } from 'react';
+
 // import axios from 'axios';
 
-export default function App()  {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [mobile, setMobile] = useState('');
-  const [caseType, setCaseType] = useState('');
-  const [description, setDescription] = useState('');
-  
+import React, { useState } from 'react';
 
-function handleSubmit(){
-   
-}
+const App = () => {
+  const [appointments, setAppointments] = useState([]);
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    mobileNumber: '',
+    caseType: '',
+    description: '',
+  });
 
- 
-  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newAppointment = { ...formData, id: Date.now() };
+    setAppointments([newAppointment, ...appointments]);
+    setFormData({
+      name: '',
+      email: '',
+      mobileNumber: '',
+      caseType: '',
+      description: '',
+    });
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
   return (
     <div className='appointment'>
-      <h1>Hospital Appointments</h1>
+      <h1> Hospital Appointments</h1>
+      
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Mobile"
-          value={mobile}
-          onChange={(e) => setMobile(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Case Type"
-          value={caseType}
-          onChange={(e) => setCaseType(e.target.value)}
-        />
-        <textarea
-          placeholder="Description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        ></textarea>
+      <center>
+        <input type="text" name="name" value={formData.name} onChange={handleChange} placeholder="Name" required />
+        <input type="email" name="email" value={formData.email} onChange={handleChange} placeholder="Email" required />
+        <input type="tel" name="mobileNumber" value={formData.mobileNumber} onChange={handleChange} placeholder="Mobile Number" required />
+        <input type="text" name="caseType" value={formData.caseType} onChange={handleChange} placeholder="Type of Case" required />
+       
+        <textarea name="description" value={formData.description} onChange={handleChange} placeholder="Description" required />
         <br></br>
         <br></br>
         <button type="submit">Book Appointment</button>
+        </center>
+        <br></br>
       </form>
+
       <h2>All Appointments</h2>
-      
+      <ul>
+        {appointments.map((appointment) => (
+          <li key={appointment.id}>
+            <p>Name: {appointment.name}</p>
+            <p>Email: {appointment.email}</p>
+            <p>Mobile Number: {appointment.mobileNumber}</p>
+            <p>Type of Case: {appointment.caseType}</p>
+            <p>Description: {appointment.description}</p>
+            <hr />
+          </li>
+        ))}
+      </ul>
     </div>
   );
+};
 
-  };
-
+export default App;
